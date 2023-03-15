e4c7934fe5f1d75c16a4faf94c99c36e93f5ada8
Ce Gao <cegao@tensorchord.ai>
2023-03-15 19:15:37 +0800

    fix: Use 8080
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -900,4 +900,4 @@ if __name__ == '__main__':
         clear.click(bot.memory.clear)
         clear.click(lambda: [], None, chatbot)
         clear.click(lambda: [], None, state)
-        demo.launch(server_name="0.0.0.0", server_port=7868)
+        demo.launch(server_name="0.0.0.0", server_port=8080)
