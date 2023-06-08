1365f593e7274fb559b2713fea8492f8e84348e9
Ce Gao <cegao@tensorchord.ai>
2023-06-08 18:18:42 +0800

    chore: Update port
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -1581,4 +1581,4 @@ if __name__ == '__main__':
         clear.click(lambda: [], None, chatbot)
         clear.click(lambda: [], None, state)
         gr.Markdown("Check out [**Visual ChatGPT**](https://github.com/microsoft/visual-chatgpt) | Hosted on [**Modelz**](https://docs.modelz.ai/)")
-    demo.launch(server_name="0.0.0.0", server_port=7861)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
